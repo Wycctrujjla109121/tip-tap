4575cf53a0a13e522dfa96d3518e4583ca7ca5c6
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-05-25 07:39:27 +0300

    Настроил RichTextEditor

--- src/app/modules/RichTextEditor/RichTextEditor.tsx
+++
@@ -1,7 +0,0 @@
-export function RichTextEditor() {
-    return (
-        <div>
-
-        </div>
-    );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import { RichTextEditor } from './modules';
+import { MantineProvider, createTheme } from '@mantine/core';
+import { RichTextEditorBoard } from './modules';
 import s from './page.module.scss'
+
+const theme = createTheme({
+  /* Put your mantine theme override here */
+});
+
 
 export default function Home() {
   return (
     <main className={s.main}>
-      <RichTextEditor />
+      <MantineProvider theme={theme}>
+        <RichTextEditorBoard />
+      </MantineProvider>
     </main>
   );
 }
